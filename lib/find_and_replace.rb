1a1155455cf0d5df4f69941da252c7e7c43f30db
Matt Carlson <matthewthomascarlson@gmail.com>
2016-06-29 15:21:17 -0700

    add files via upload

--- lib/find_and_replace.rb
+++ lib/find_and_replace.rb
@@ -2,17 +2,30 @@ require('pry')
 
 class String
   define_method(:find_and_replace) do |word1, word2|
-    word_array = self.split(' ')
+    word_array = self.downcase().split(' ')
     new_word_array = []
+    word1_lower = word1.downcase()
+    word2_lower = word2.downcase()
     word_array.each do |word|
-      if word == word1
-        # word = word2
-        new_word_array.push(word2)
+      if word.include?(word1_lower)
+        position = word.index(word1_lower)
+        value_to_add = word1_lower.length - 1
+        end_position = position + value_to_add
+        word[position..end_position] = word2_lower
+        if word == 'i'
+          word.capitalize!()
+        end
+        new_word_array.push(word)
       else
+        if word == 'i'
+          word.capitalize!()
+        end
         new_word_array.push(word)
       end
-      # new_word_array.concat(word)
     end
+    first_word = new_word_array.shift()#.capitalize()
+    first_word.capitalize!()
+    new_word_array.unshift(first_word)
     new_word_array.join(' ')
   end
 end
